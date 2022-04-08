865e6e15c5f583a4c3daac187e20353703f27c02
benipalkaranpreet <66537041+benipalkaranpreet@users.noreply.github.com>
2022-04-07 21:51:03 -0400

    Create maze.h

---
+++ maze.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+class maze {
+	int height;
+	int width;
+	int* cells;
+	int startPos;
+
+	char neighbor(int pos) const;
+	void mazeGen();
+public:
+
+	// Generates a maze of given height and width (using recursive back-tracking algorithm)
+	maze(int height, int width);
+	~maze();
+	// Prints the maze with ascii block characters 
+	void mazePrint() const;
+};
